3f94d21f18802dd933f97b6af9d0788c858e3130
Littl3F0x <artur.computacional@gmail.com>
2022-04-04 20:09:52 -0300

    Project

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+# For this project i use concatenation of strings to replicate the game 
+# A few ways to do this 
+
+# programing_language="Python"
+
+# print("I'm practicing "+programing_language) 
+# print("I'm practicing {}".format(programing_language))
+# print(f"I'm practicing {programing_language}")
+
+
+import string
+
+
+while True:
+    try:
+        verb = input(str("A verb: "))
+        adjective = input(str("A adjective: "))
+    except:
+        print("You did something wrong!! Try again!")
+        break
+    else:
+        madlibs=f"Today was a {adjective} day to {verb}!\
+ Today is a awesome day"
+
+        print(madlibs)
+
+        again = input(str("Would you like to play again S/N?:"))
+        if(again != "S"):
+            break
